6bb8695b9149b4e5e71d62c43b89456e3bb3ef4c
rushed <rushed@users.noreply.github.com>
2021-06-03 00:37:00 -0500

    Patent updates.

--- lib/reference_parser/patent.rb
+++ lib/reference_parser/patent.rb
@@ -2,11 +2,12 @@ class ReferenceParser::Patent < ReferenceParser::Base
   replace(/Patent Number (?<number>[0-9,]+)/)
 
   def url(citation, url_options = {})
+    return unless citation[:number]&.present?
     "http://patft.uspto.gov/netacgi/nph-Parser?Sect2=PTO1&Sect2=HITOFF&p=1&u=/netahtml/PTO/search-bool.html&r=1&f=G&l=50&d=PALL&RefSrch=yes&Query=PN/#{citation[:number]}"
   end
 
   def clean_up_named_captures(captures, options: {})
-    captures[:number] = captures[:number].delete(",")
+    captures[:number] = captures[:number]&.delete(",")
   end
 
   def slug
