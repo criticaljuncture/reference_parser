a2c623e422a08d0f7ebf597a933f529711c7a3c4
rushed <rushed@users.noreply.github.com>
2023-11-20 18:02:37 -0600

    Update space handling

--- lib/reference_parser/spaces.rb
+++ lib/reference_parser/spaces.rb
@@ -1,19 +1,24 @@
 class ReferenceParser::Spaces
   THIN_SPACE = " "
+  EM_SPACE = " "
+  EN_SPACE = " "
+  NON_BREAKING_SPACE = " "
 
-  SPACES = [" ", THIN_SPACE]
+  SPACES = [" ", EM_SPACE, EN_SPACE, THIN_SPACE]
 
   def self.ascii(text)
     simplify(text, " ")
   end
 
   def self.complex?(text)
-    text.include?(THIN_SPACE)
+    SPACES.detect { |space| text&.include?(space) }
   end
 
   def self.simplify(text, replacement = nil)
     return text unless text.present?
     text = text.to_s unless text.is_a?(String)
-    text&.tr(THIN_SPACE, replacement || " ")
+    text = text&.tr(THIN_SPACE, replacement || " ")
+    text = text&.tr(EM_SPACE, replacement || " ")
+    text&.tr(EN_SPACE, replacement || " ")
   end
 end
